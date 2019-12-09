f695fa238d4d134748856396f9cc8e98b4d3216e
Divyendu Singh <singh@prisma.io>
2019-12-09 15:06:28 +0100

    init

--- functions/hello-facade/hello-facade.js
+++ functions/hello-facade/hello-facade.js
@@ -1,12 +1,26 @@
 const { Photon } = require('@prisma/photon')
+const { spawn } = require('child_process')
 
 const photon = new Photon()
 
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
 exports.handler = async (event, context) => {
   try {
+    const os = spawn('cat', ['/etc/os-release'])
+    os.stdout.on('data', data => {
+      console.log(`stdout: ${data}`)
+    })
+
+    os.stderr.on('data', data => {
+      console.error(`stderr: ${data}`)
+    })
+
+    os.on('close', code => {
+      console.log(`child process exited with code ${code}`)
+    })
+
     const data = await photon.users.findMany()
-    console.log({data})
+    console.log({ data })
     return {
       statusCode: 200,
       body: JSON.stringify({ message: data }),
