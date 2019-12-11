58202f343df00e84ec5cda42aeb8335c7df36106
timsuchanek <tim.suchanek@gmail.com>
2019-12-11 10:53:59 +0100

    update facade

--- functions/hello-facade/hello-facade.js
+++ functions/hello-facade/hello-facade.js
@@ -1,25 +1,18 @@
 const { Photon } = require('@prisma/photon')
-process.env.DEBUG = '*'
 const { getPlatform } = require('@prisma/get-platform')
-const globby = require('globby')
 
-// const photon = new Photon()
-
-if (false) {
-  require('@prisma/photon/runtime/query-engine-rhel-openssl-1.0.x')
-}
+const photon = new Photon()
 
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
 exports.handler = async (event, context) => {
   try {
     const platform = await getPlatform()
     console.log(platform)
-    const files = await globby(['node_modules/@prisma/photon/**/*'])
-    // const data = await photon.users.findMany()
+    const data = await photon.users.findMany()
     // console.log({ data })
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: platform, files }),
+      body: JSON.stringify({ data }),
       // // more keys you can return:
       // headers: { "headerName": "headerValue", ... },
       // isBase64Encoded: true,
